99241c1401fca6d47e277262d9e8be6396b8dc16
Ethan N <ejnephew@yahoo.com>
2021-05-23 20:37:23 -0400

    Primitive types.

---
+++ src/Java_Primitive_Types.java
@@ -0,0 +1,28 @@
+/***
+ * #1
+ * Review Java Primitive Types - With Archibald (https://www.youtube.com/watch?v=bPLqGal8_2Y&t=976s)
+ */
+public class Java_Primitive_Types {
+
+    /***
+     * Generic main class for reviewing the 8 primitive types.
+     * @param primitives
+     */
+    public static void main(String[] primitives){
+
+        // Promotion order of primitive data types is: byte, short, int, long, float, and then double.
+        // It is important to know the order, especially when using mixed type expressions.
+
+        byte b;     // 1 byte
+        short s;    // 2 bytes
+        int i;      // 4 bytes
+        long lng;   // 8 bytes
+
+        float fl;   // 4 bytes
+        double dbl; // 8 bytes
+
+        boolean bool; // 1 bytes
+        char ch;      // 2 bytes
+
+    }
+}
