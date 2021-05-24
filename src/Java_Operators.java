7ad6f6f8bce441cea39dcd172f28d441be5d2687
Ethan N <ejnephew@yahoo.com>
2021-05-24 12:08:01 -0400

    Operators.

--- src/Java_Operators.java
+++ src/Java_Operators.java
@@ -1,4 +1,4 @@
-
+import org.w3c.dom.ls.LSOutput;
 
 /***
  * #2
@@ -6,4 +6,37 @@
  */
 public class Java_Operators {
 
+
+    /*
+    Arithmetic Operators are:
+    + - * / %
+
+    Comparators/Relational Operators are:
+    > < >= <= == !=
+
+    Logical Operators are:
+    && & || | ! ^
+
+    Bitwise Operators are:
+    >> >>> << ~
+
+    Pre and Post Assignments are:
+    ++ --
+
+    Combined Assignments are:
+    += -= *= %= /=
+
+    Unary Operators are: (these are in the Java language, but they do nothing)
+    + -
+
+    Cast Operators are:
+    (type) (int) (double)
+    */
+    public static void main(String[] args){
+        System.out.println(15|8);
+        System.out.println(+45); // does nothing
+
+        int a = 10, b = 6;
+        double dbl = 1.5;
+    }
 }
