5ed64f810d376033c18b11cb9919bae4ec72be0f
Ethan N <ejnephew@yahoo.com>
2021-05-24 16:41:22 -0400

    Control structures.

--- src/Java_Control_Structures.java
+++ src/Java_Control_Structures.java
@@ -6,8 +6,19 @@ public class Java_Control_Structures {
     // Selection: if else, switch, ? : (conditional operator)
     // Repetition: for, do while, while, enhanced for loop
 
-    public void main(String[] args){
+    public static void main(String[] args){
         int a = 10, b = 6, c;
+
+
+        /*
+        Conditional operator looks kind of weird.
+        The syntax is: (if true) ? do this : else do this
+                        a > b    ?     5   :     35
+        */
+        c = a > b ? 23 : a / b;
+        System.out.println(c);
+        System.out.println(a > b ? 5 : 35); // this evaluates to a int 5
+        System.out.println(a > b ? 5 : 35.5); // this evaluates to a double 5.0
 
         /*
         Always use proper indentation. Always use { }.
@@ -16,6 +27,27 @@ public class Java_Control_Structures {
             a = 10;
         } else {
             System.out.println("else");
+        }
+
+        switch (a % 2){
+            case 0:
+                System.out.println("It is a 0");
+                break;
+            case 1:
+                System.out.println("It is a 1");
+                break;
+            default:
+                System.out.println("Not 1 or 2");
+                break;
+        }
+
+        switch("Colin"){
+            case "Bob":
+                System.out.println("Bob");
+                break;
+            case "Colin":
+                System.out.println("Colin");
+                break;
         }
     }
 }
