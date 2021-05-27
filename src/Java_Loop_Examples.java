290cfe9a899b56c2f380b9fd93ba2573b6ab99b1
Ethan N <ejnephew@yahoo.com>
2021-05-27 10:23:05 -0400

    Some simple loops.

--- src/Java_Loop_Examples.java
+++ src/Java_Loop_Examples.java
@@ -1,9 +1,17 @@
-
-
+/***
+ * #4
+ * Just completing a few very basic loop challenges. 
+ */
 public class Java_Loop_Examples {
 
     public static void main(String[] args){
-        int counter = 0;
+
+        /*
+        When making a loop, I need to know the answer to 3 questions.
+        1. Where do I start?
+        2. Where do I stop?
+        3. How do I get there?
+        */
 
         // output the multiples of 5 less than 100
         for(int i = 5; i < 100; i++){
@@ -20,6 +28,7 @@ public class Java_Loop_Examples {
         }
 
         // sum the numbers from 1 to 100, and output the result.
+        int counter = 0;
         for(int i = 0; i < 'd'; i++){
             counter+=i;
         }
