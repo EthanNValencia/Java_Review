65c47afc3fb1e327b362827d4ebe420a5a0e5a61
Ethan N <ejnephew@yahoo.com>
2022-06-14 13:24:34 -0400

    Uploading changes.

--- src/LevenshteinDistance.java
+++ src/LevenshteinDistance.java
@@ -16,13 +16,7 @@ public class LevenshteinDistance {
             }
             edits[i][0] = i;
         }
-        for(int i = 0; i < str2.length() + 1; i++) {
-            for(int j = 0; j < str1.length() + 1; j++) {
-                System.out.print(edits[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+
         for(int i = 1; i < str2.length() + 1; i++) {
             for(int j = 1; j < str1.length() + 1; j++) {
                 if(str2.charAt(i - 1) == str1.charAt(j - 1)) {
@@ -32,13 +26,24 @@ public class LevenshteinDistance {
                 }
             }
         }
+        print(edits, str1, str2);
+        return edits[str2.length()][str1.length()];
+    }
+
+    public static void print(int[][] edits, String str1, String str2) {
+        String word1 = "  " + str1;
+        String word2 = " " + str2;
+        for(int i = 0; i < word1.length(); i++) {
+            System.out.print(word1.charAt(i) + " ");
+        }
+        System.out.println();
         for(int i = 0; i < str2.length() + 1; i++) {
+            System.out.print(word2.charAt(i) + " ");
             for(int j = 0; j < str1.length() + 1; j++) {
                 System.out.print(edits[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println();
-        return edits[str2.length()][str1.length()];
     }
+
 }
